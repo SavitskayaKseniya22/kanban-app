7f526cc57e77aec2fd99ac022dd9381239503cf4
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2023-09-18 16:10:38 +0300

    refactor: spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,28 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledSpinner = styled('div')`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  .active-search {
-    border: 5px dotted red;
-    border-radius: 50%;
-    flex-grow: unset;
-    width: 50px;
-    height: 50px;
-    animation: move 5s infinite linear;
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  }
-
-  .non-visible {
-    display: none;
-  }
+  border: 5px dotted red;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: move 5s infinite linear;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
   @keyframes move {
     0% {
@@ -40,13 +27,7 @@ const StyledSpinner = styled('div')`
 `;
 
 function Spinner() {
-  return (
-    <StyledSpinner>
-      <div className="active-search">
-        <span className="non-visible">Loading data</span>
-      </div>
-    </StyledSpinner>
-  );
+  return <StyledSpinner />;
 }
 
 export default Spinner;
